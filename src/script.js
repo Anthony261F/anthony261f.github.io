9dee289ed71c296983f25461d33db40eb3f23ba0
Anthony261 <jeux.anthony@free.fr>
2023-06-23 14:54:44 +0200

    qcw

--- src/script.js
+++ src/script.js
@@ -1,22 +1,22 @@
 const socket = new WebSocket('ws://localhost:2023');
-let lastActivityTime = Date.now(); // Enregistrez cette valeur lorsque l'utilisateur se connecte
+let nomc = ''; // Déclarer la variable nomc à l'extérieur de la fonction envoie_nom
 
 socket.onopen = function() {
-    console.log('Connecté au serveur WebSocket');
+  console.log('Connecté au serveur WebSocket');
 };
 
 socket.onmessage = function(event) {
-    const message = document.createElement('div');
-    message.textContent = event.data;
-    document.getElementById('messages').appendChild(message);
+  const message = document.createElement('div');
+  message.textContent = event.data;
+  document.getElementById('messages').appendChild(message);
 };
 
 document.getElementById('formulaire').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const message = document.getElementById('message').value;
-    const lenom = document.getElementById('nom').value + " : ";
-    socket.send(lenom + message);
-    document.getElementById('message').value = '';
+  event.preventDefault();
+  const message = document.getElementById('message').value;
+  const lenom = document.getElementById('nom').value + " : ";
+  socket.send(lenom + message);
+  document.getElementById('message').value = '';
 });
 
 var nom_b = document.getElementById("fond");
@@ -24,24 +24,30 @@ var nom_t = document.getElementById("fond2");
 nom_b.style.visibility = "visible";
 nom_t.style.visibility = "visible";
 function envoie_nom() {
-    nom_b.style.visibility = "hidden";
-    nom_t.style.visibility = "hidden";
-    document.getElementById('renom').innerText = document.getElementById('nom').value;
-    const nomc = document.getElementById('nom').value;
-    socket.send("[+] " + nomc + " à rejoint le tchat");
+  nom_b.style.visibility = "hidden";
+  nom_t.style.visibility = "hidden";
+  document.getElementById('renom').innerText = document.getElementById('nom').value;
+  nomc = document.getElementById('nom').value; // Affecter la valeur à la variable nomc
+  socket.send("[+] " + nomc + " a rejoint le tchat");
 }
 
-document.addEventListener('mousemove', function() {
-    lastActivityTime = Date.now();
-  });
+let lastActivityTime = Date.now(); // Enregistrez cette valeur lorsque l'utilisateur se connecte
 
-  setInterval(function() {
-    const currentTime = Date.now();
-    const inactivityThreshold = 120000; // Durée d'inactivité en millisecondes (2 minutes)
-  
-    if (currentTime - lastActivityTime > inactivityThreshold) {
-      // Utilisateur considéré comme déconnecté
-      socket.send("[-] " + nomc + " à quitté le tchat");
+document.documentElement.addEventListener('mousemove', function() {
+  lastActivityTime = Date.now();
+});
+
+setInterval(function() {
+  const currentTime = Date.now();
+  const inactivityThreshold = 30000; // Durée d'inactivité en millisecondes (2 minutes)
+
+  if (currentTime - lastActivityTime > inactivityThreshold) {
+    // Utilisateur considéré comme déconnecté
+    if (nomc = "") {
+
+    } else {
+        socket.send("[-] " + nomc + " a quitté le tchat");
+        location.reload();        
     }
-  }, 60000); // Vérification toutes les minutes
-  
+  }
+}, 10000); // Vérification toutes les 10 secondes
