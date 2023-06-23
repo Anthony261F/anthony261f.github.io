45848908a8b47872f3d21ecc491636b7253f8659
Anthony261 <jeux.anthony@free.fr>
2023-06-23 12:05:42 +0200

    sfsdv

--- src/script.js
+++ src/script.js
@@ -13,6 +13,17 @@ socket.onmessage = function(event) {
 document.getElementById('formulaire').addEventListener('submit', function(event) {
     event.preventDefault();
     const message = document.getElementById('message').value;
-    socket.send(message);
+    const lenom = document.getElementById('nom').value + " : "
+    socket.send(lenom + message);
     document.getElementById('message').value = '';
 });
+
+var nom_b = document.getElementById("fond");
+var nom_t = document.getElementById("fond2");
+nom_b.style.visibility = "visible";
+nom_t.style.visibility = "visible";
+function envoie_nom() {
+    nom_b.style.visibility = "hidden";
+    nom_t.style.visibility = "hidden";
+    document.getElementById('renom').innerText = document.getElementById('nom').value
+}
