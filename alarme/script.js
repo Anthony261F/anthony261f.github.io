1de0a1f2d6d7e413481bf87af27fca03a708de5b
Anthony261 <jeux.anthony@free.fr>
2023-06-26 13:06:26 +0200

    sC

--- alarme/script.js
+++ alarme/script.js
@@ -2,15 +2,17 @@ var datedepart2 = "";
 var temps2 = "";
 var dateactuelle = "";
 var repete2 ="";
+let audio = new Audio("alarme.mp3");
 const timerElement = document.getElementById("timer");
 function start2() {
     datedepart2 = Date.now();
     repete2 = setInterval(() => {
         dateactuelle = Date.now();
         temps2 = Math.round((dateactuelle - datedepart2) / 1000);
-        temps2 = 7200 - temps2;
+        temps2 = 10 - temps2;
         timerElement.innerText = temps2;
         if (temps2 < 1) {
+            audio.play();
             alert("timer fini");
             clearInterval(repete2);
         }
